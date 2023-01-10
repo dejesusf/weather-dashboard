7b6f616d97883b2782fe59892975801a6bff521c
Feliclare De Jesus <feliclare@Feliclares-MacBook-Pro.local>
2023-01-10 13:03:06 -0800

    created a basic html layout with som e css to modify colors and text styles

---
+++ script.js
@@ -0,0 +1,7 @@
+//API Key
+var APIKey= "060e7e9eba72369f7f70faf21f68c505";
+
+//variables
+// var queryURL= "http://api.openweathermap.org/data/2.5/weather?q="  city  "&appid="  APIKey";
+
+fetch(queryURL)
