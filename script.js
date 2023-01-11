16b9063547a51aaee431f58aa3c1f27144af2ec3
Feliclare De Jesus <feliclare@Feliclares-MacBook-Pro.local>
2023-01-11 02:56:04 -0800

    fixed search button and completed localstorage for input

--- script.js
+++ script.js
@@ -2,24 +2,49 @@
 var APIKey= "060e7e9eba72369f7f70faf21f68c505";
 
 //variables
-var searchInput= document.querySelector("#search");
-var searchBtn= document.querySelector("#search-button");
-var currentCityEl= document.querySelector("#current-city");
-//temperature
-//humidity
-//windspeed
-//date
+var searchInput= document.getElementById("search");
+var searchBtn= document.getElementById("search-button");
+var cityList= document.getElementById("list-buttons")
+var currentCityEl= document.getElementById("current-city");
+var citySel= document.getElementById("city");
+var cityTemp= document.getElementById("temp");
+var cityWind= document.getElementById("wind");
+var cityHumidity=document.getElementById("humidity");
 //icon for weather
 //temp, humidity, weather for 5 day forecast
-
-
 var queryURL= ('https://api.openweathermap.org/data/2.5/weather?q={input}&appid={APIKey}')
 
-//save input in localStorage
+//search history
+//save input in localStorage (set, get)
+function saveSearchHistory() {
+    var citySearch= searchInput.value;
+    localStorage.setItem("city-search", JSON.stringify(citySearch)); 
+}
 
+//display input from localStorage as a list below search input
+function renderSearchHistory() {
+    var cityValue=JSON.parse(localStorage.getItem('city-search'))
+    if (cityValue !== null) {
+       var newLi= document.createElement("li");
+            cityList.appendChild(newLi);
+            var newBtn= document.createElement("button");
+            newBtn.innerHTML=cityValue;
+            newLi.appendChild(newBtn);
+
+    } else {
+      return;
+    }
+}
+
+//clear input form
+function clearSubmit() {
+    document.getElementById("search").value=('');
+}
 
 //event listener for search button
-
-
-//fetch
-// fetch(queryURL)
+searchBtn.addEventListener("click", function (event){
+    event.preventDefault();
+    saveSearchHistory();
+    renderSearchHistory();
+    clearSubmit();
+})
