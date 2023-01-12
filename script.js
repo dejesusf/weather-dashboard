3cd9a42b6ba8475950ea6e4f2d29ad019c8f7309
Feliclare De Jesus <feliclare@Feliclares-MacBook-Pro.local>
2023-01-11 20:39:02 -0800

    attempted to fix fetch but was unsuccessful

--- script.js
+++ script.js
@@ -7,32 +7,49 @@ var searchBtn= document.getElementById("search-button");
 var cityList= document.getElementById("list-buttons")
 var currentCityEl= document.getElementById("current-city");
 var citySel= document.getElementById("city");
+var today= document.getElementById("date");
 var cityTemp= document.getElementById("temp");
 var cityWind= document.getElementById("wind");
 var cityHumidity=document.getElementById("humidity");
 //icon for weather
 //temp, humidity, weather for 5 day forecast
-var queryURL= ('https://api.openweathermap.org/data/2.5/weather?q={input}&appid={APIKey}')
+var queryURL= ('https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${APIKey}')
 
 //search history
 //save input in localStorage (set, get)
 function saveSearchHistory() {
     var citySearch= searchInput.value;
+
     localStorage.setItem("city-search", JSON.stringify(citySearch)); 
 }
 
 //display input from localStorage as a list below search input
 function renderSearchHistory() {
-    var cityValue=JSON.parse(localStorage.getItem('city-search'))
-    if (cityValue !== null) {
-       var newLi= document.createElement("li");
-            cityList.appendChild(newLi);
-            var newBtn= document.createElement("button");
-            newBtn.innerHTML=cityValue;
-            newLi.appendChild(newBtn);
+    var cityValue=JSON.parse(localStorage.getItem('city-search'));
 
-    } else {
-      return;
+    if (cityValue !== "") {
+        var newLi= document.createElement("li");
+
+        cityList.appendChild(newLi);
+        var newBtn= document.createElement("button");
+
+        newBtn.innerHTML=cityValue;
+        newLi.appendChild(newBtn);
+        
+        fetch ('https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${APIKey}', {
+            cache:'reload',
+        })
+        .then(function (response) {
+            return response.JSON();
+        })
+        .then (function (data) {
+            console.log(data);
+            var date= dayjs().format('MMMM D, YYYY');
+            
+            citySel.textContent(data.name);
+            today.textContent('Date: '+date);
+            cityTemp.textContent('Temp: '+data.temp);
+            cityWind.textContent('Wind Speed: '+data.wind.speed);cityHumidity.textContent('Humidity: '+data.main.humidity);})
     }
 }
 
@@ -40,6 +57,12 @@ function renderSearchHistory() {
 function clearSubmit() {
     document.getElementById("search").value=('');
 }
+
+//setInterval for time
+setInterval(function(){
+    var dateTime= dayjs().format('MMMM D, YYYY [at] HH:mm:ss');
+    $('#currentTime').text(dateTime);
+  }, 1000);
 
 //event listener for search button
 searchBtn.addEventListener("click", function (event){
